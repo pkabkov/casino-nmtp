61b2befda729fbe88639e05c8403bb402e03d60f
MishanyaX <michasergeev@yandex.ru>
2026-01-02 04:52:31 +0300

    Настроена форма регистрации с использованием бэкенд

--- nmtp-front/components/app/signup-card.vue
+++ nmtp-front/components/app/signup-card.vue
@@ -5,41 +5,26 @@ import { useSignupErrorMessage } from '~/composables/useSignupErrors'
 const username = ref('')
 const password = ref('')
 const passwordConfirm = ref('')
-const errors = ref<{ username?: string, password?: string, passwordConfirm?: string }>({})
-const userId = ref('')
-const response = ref('')
 const error = ref('')
-
-function resetForm() {
-  username.value = ''
-  password.value = ''
-  passwordConfirm.value = ''
-  errors.value = {}
-  response.value = ''
-  error.value = ''
-  userId.value = ''
-}
 
 async function onSubmit() {
   try {
-    const res = await $fetch('/api/signup', {
+    const res = await $fetch('/api/register', {
       method: 'POST',
       body: {
-        username: username.value,
-        password: password.value,
-        passwordConfirm: passwordConfirm.value
+        login: username.value.trim(),
+        password: password.value.trim(),
+        passwordConfirm: passwordConfirm.value.trim(),
       }
     })
-    response.value = res.message 
-    userId.value = res.id
-    useState('isSignedUp').value = true
-    navigateTo({
-      name: 'users-id',
-      params: {
-        id: userId.value,
-      }
-    })
-    resetForm()
+
+    const session = useUserSession()
+    await session.fetch()
+
+    const route = useRoute()
+    const redirect = route.query.redirect as string || '/'
+
+    return navigateTo(redirect)
 
   } catch (err: any) {
     error.value = useSignupErrorMessage(err)
---
+++ nmtp-front/server/api/register.post.ts
@@ -0,0 +1,56 @@
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event)
+  const { login, password, passwordConfirm } = body
+
+  if (!passwordConfirm || !password || !login) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Missing username or password or passwordConfirm'
+    })
+  }
+
+  try{
+    const bodyToSend: Record<string, string> = {}
+
+    if (login !== '') bodyToSend.login = login
+    if (password !== '') bodyToSend.password = password
+
+    const res = await $fetch<{ login: string }>(
+      'http://localhost:8080/api/register',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: bodyToSend
+      }
+    )
+    let balanceInfo = await $fetch(
+      `/api/users/${res.login}`,
+      {
+        method: 'POST'
+      }
+    )
+
+    console.log(balanceInfo.balance ?? "Пусто")
+
+    await setUserSession(event, {
+      user: { id: res.login, balance: balanceInfo.balance },
+      secure: { apiToken: '1234567890' }
+    })
+
+  } catch (err: any) {
+
+    const status = err?.status || err?.statusCode || err?.response?.status
+    const serverMessage =
+      err?.data?.message || err?.response?.data?.message || err?.message || err?.statusMessage
+
+    throw createError({ statusCode: status, statusMessage: serverMessage })
+  }
+
+  if (password !== passwordConfirm) {
+    throw createError({
+      statusCode: 444,
+      statusMessage: 'Passwords don`t match'
+    })
+  }
+  
+})
